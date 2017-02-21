9759399ab1eb915cf102d389cd93371779972f16
Ludovic Robez <ludovic.robez@unice.fr>
2017-02-21 11:08:17 +0100

    Implementation du service SecurityServices

--- server/NFCPaiement/src/main/java/fr/Security/Services/SecurityServicesImpl.java
+++ server/NFCPaiement/src/main/java/fr/Security/Services/SecurityServicesImpl.java
@@ -1,13 +1,24 @@
 package fr.Security.Services;
 
+import fr.Security.AES.Cryptography;
+import fr.Security.RSA.RSAKeyManagement;
+
 import javax.ws.rs.core.Response;
 
 /**
  * Created by rl613611 on 21/02/2017.
  */
 public class SecurityServicesImpl implements SecurityServices {
+
+   private static final String publicKeyFile = "";
+
     @Override
     public Response getPublicKey() {
-        return null;
+        try {
+            String encodedPublicKey =Cryptography.Chiffrement(RSAKeyManagement.lectureClePublique(publicKeyFile).toString());
+            return Response.ok(encodedPublicKey).build();
+        }catch (Exception e){
+            return Response.status(404).build();
+        }
     }
 }
