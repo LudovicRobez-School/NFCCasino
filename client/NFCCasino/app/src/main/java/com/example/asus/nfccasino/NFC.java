48f8fca3b698e4191acabe0671aa4f5cbb133142
GregoryVesic <gregory.vesic@etu.unice.fr>
2017-04-04 14:22:05 +0200

    [US-1.3 & 6.1] Inscription, Profil, Créditer, NFC
    Inscription, profil et créditage en dur.
    Envoie de données NFC fonctionnel. (testé entre 2 smartphones Android).

---
+++ client/NFCCasino/app/src/main/java/com/example/asus/nfccasino/NFC.java
@@ -0,0 +1,18 @@
+package com.example.asus.nfccasino;
+
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+
+import java.nio.charset.Charset;
+import java.util.Locale;
+
+/**
+ * Created by Asus on 28/03/2017.
+ */
+
+public class NFC {
+
+    NFC() {};
+
+
+}
---
+++ client/NFCCasino/app/src/main/java/com/example/asus/nfccasino/User.java
@@ -0,0 +1,158 @@
+package com.example.asus.nfccasino;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+class User implements Parcelable {
+
+    private String login;
+    private String token;
+    private String pseudo;
+    private int score;
+    private String team;
+    private String email;
+    private String name;
+    private String firstname;
+
+    User(String login) {
+        this.login = login;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+
+    public void setLogin(String login){
+        this.login=login;
+    }
+
+    public String getToken(){
+        return token;
+    }
+
+    public String getPseudo(){
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo){
+        this.pseudo=pseudo;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(int score){
+        this.score=score;
+    }
+
+    public String getTeam(){
+        return team;
+    }
+
+    public void setTeam(String team){
+        this.team=team;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getFirstname(){
+        return firstname;
+    }
+
+    public void setFirstname(String firstname){
+        this.firstname=firstname;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(login);
+        out.writeString(token);
+        out.writeString(pseudo);
+        out.writeInt(score);
+        out.writeString(team);
+        out.writeString(email);
+        out.writeString(name);
+        out.writeString(firstname);
+    }
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    private User(Parcel in) {
+        this.login = in.readString();
+        this.token = in.readString();
+        this.pseudo = in.readString();
+        this.score = in.readInt();
+        this.team = in.readString();
+        this.email = in.readString();
+        this.name = in.readString();
+        this.firstname = in.readString();
+    }
+
+    public void initProfil(User user) {
+        if(!user.getInfo(user.getLogin()).equals("")&&!user.getInfo(user.getLogin()).equals("-1")) {
+            try {
+                JSONObject jsonRanking = new JSONObject(user.getInfo(user.getLogin()));
+                    user.setPseudo(jsonRanking.getString("pseudo"));
+                    user.setScore(jsonRanking.getInt("score"));
+                    user.setTeam(jsonRanking.getString("team"));
+                    user.setEmail(jsonRanking.getString("email"));
+                    user.setName(jsonRanking.getString("lastName"));
+                    user.setFirstname(jsonRanking.getString("firstName"));
+            } catch (JSONException e) {
+                Log.i("error json", e.toString());
+            }
+        }
+    }
+
+    public String getInfo(String login) {
+        String URL  = "" + login;
+        Http requete = new Http();
+        requete.execute("GET",URL,"");
+        String result ="";
+        try {
+            result = requete.get();
+        }
+        catch(Exception e ){
+            Log.i("error", e.toString());
+        }
+        if(!result.equals("")) {
+            return result;
+        } else {
+            return "Erreur de récupération des informations";
+        }
+    }
+}
