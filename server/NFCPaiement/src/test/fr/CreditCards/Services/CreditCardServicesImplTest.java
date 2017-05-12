08797a8d871e31ea2107102a08c1f71586595514
LudovicRobez <ludovic.robez@hotmail.fr>
2017-05-12 16:18:54 +0200

    Ajout des test de CreditCardServices

--- server/NFCPaiement/src/test/fr/CreditCards/Services/CreditCardServicesImplTest.java
+++ server/NFCPaiement/src/test/fr/CreditCards/Services/CreditCardServicesImplTest.java
@@ -1,18 +1,23 @@
 package fr.CreditCards.Services;
 
 import fr.CreditCards.Providers.CreditCardProviders;
-import fr.CreditCards.Ressources.CreditCard;
 import fr.Security.RSA.Cryptography;
 import junit.framework.TestCase;
+import org.codehaus.jettison.json.JSONObject;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,13 +37,20 @@ public class CreditCardServicesImplTest {
         return new ResourceConfig(CreditCardServicesImpl.class);
     }
 
+    Client client;
+
+    @Before
+    public void setUp() throws Exception {
+        client = ClientBuilder.newClient();
+    }
+
     @Test
     public void getAllCreditCards() throws Exception {
 
         PowerMock.mockStatic(CreditCardProviders.class);
-        expect(CreditCardProviders.getAllCreditCardsById(anyInt())).andReturn(new ArrayList<Map<String, String>>());
+        expect(CreditCardProviders.getAllCreditCardsById(anyString())).andReturn(new ArrayList<Map<String, String>>());
 
-        Response output = target("/creditcard/" + Cryptography.chiffrementRSA("1")).request().get();
+        Response output = client.target("http://localhost:8080/nfcpaiement/creditcard/" + Cryptography.chiffrementRSA("1")).request().get();
 
         Assert.assertEquals("should return status 200", 200, output.getStatus());
 
@@ -49,20 +61,21 @@ public class CreditCardServicesImplTest {
     public void addCreditCard() throws Exception {
 
         PowerMock.mockStatic(CreditCardProviders.class);
-        expect(CreditCardProviders.insertCreditCard(anyInt(), anyString(), anyInt(), anyInt(), anyString())).andReturn(true);
+        expect(CreditCardProviders.insertCreditCard((Map<String, String>) anyObject())).andReturn(true);
+        JSONObject json = new JSONObject(new HashMap<String, String>());
 
-        Response output = target("/creditcard").request().post();
+        Response output = client.target("http://localhost:8080/nfcpaiement/creditcard").request().post(Entity.entity(MediaType.APPLICATION_JSON_TYPE,json.toString()));
 
-        Assert.assertEquals("should return status 202", 202, output.getStatus());
+        Assert.assertEquals("should return status 201", 201, output.getStatus());
     }
 
     @Test
     public void getCreditCard() throws Exception {
 
         PowerMock.mockStatic(CreditCardProviders.class);
-        expect(CreditCardProviders.findCreditCardById(anyInt(),anyInt())).andReturn(new HashMap<String, String>());
+        expect(CreditCardProviders.findCreditCardById(anyInt(),anyString())).andReturn(new HashMap<String, String>());
 
-        Response output = target("/creditcard").request().get();
+        Response output = client.target("http://localhost:8080/nfcpaiement/creditcard").request().get();
 
         Assert.assertEquals("should return status 200", 200, output.getStatus());
 
@@ -74,7 +87,7 @@ public class CreditCardServicesImplTest {
         PowerMock.mockStatic(CreditCardProviders.class);
         expect(CreditCardProviders.deleteCreditCard(anyInt())).andReturn(true);
 
-        Response output = target("/creditcard").request().delete();
+        Response output = client.target("http://localhost:8080/nfcpaiement/creditcard").request().delete();
 
         Assert.assertEquals("should return status 200", 200, output.getStatus());
     }
