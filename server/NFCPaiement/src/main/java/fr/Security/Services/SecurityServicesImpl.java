a6baa476f38370e3e0e98f13d883d05159039ab6
LudovicRobez <ludovic.robez@hotmail.fr>
2017-05-12 15:16:15 +0200

    Modification du service de Security

--- server/NFCPaiement/src/main/java/fr/Security/Services/SecurityServicesImpl.java
+++ server/NFCPaiement/src/main/java/fr/Security/Services/SecurityServicesImpl.java
@@ -1,6 +1,7 @@
 package fr.Security.Services;
 
 import fr.Security.AES.Cryptography;
+import fr.Security.RSA.RSAKeyGenerator;
 import fr.Security.RSA.RSAKeyManagement;
 
 import javax.ws.rs.Path;
@@ -17,10 +18,11 @@ public class SecurityServicesImpl implements SecurityServices {
     @Override
     public Response getPublicKey() {
         try {
+            RSAKeyGenerator.GeneratingKeys("","");
             String encodedPublicKey = Cryptography.chiffrementAES(RSAKeyManagement.lectureClePublique(publicKeyFile).toString());
             return Response.ok(encodedPublicKey).build();
         }catch (Exception e){
-            return Response.status(404).build();
+            return Response.status(204).build();
         }
     }
 }
